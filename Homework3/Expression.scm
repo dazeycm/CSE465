0103dcc8f64212e0607f8ebf057064563ff1e331
dazeycm <craigdazey@gmail.com>
2015-03-09 05:50:44 -0400

    added and pretty-fied printing

--- Homework3/Expression.scm
+++ Homework3/Expression.scm
@@ -13,22 +13,6 @@
   )
 )
 
-;(define (testtestest numTrees lessthan between greaterthan)
- ;   (cond
-  ;      ((= numTrees 0) (display "Goodbye!"))
-    ;    (else(let()(begin 
-     ;           (define y (createTree 2))
-      ;          (cond
-       ;             ((< (eval y user-initial-environment) (- 25)) (testtestest (- numTrees 1) (+ 1 lessthan) between greaterthan))
-        ;            ((> (eval y user-initial-environment) 25) (testtestest (- numTrees 1) lessthan between (+ 1 greaterthan)))
-         ;           (else (testtestest (- numTrees 1) lessthan (+ 1 between) greaterthan))
-          ;       )
-           ;     )
-           ; )
-;        )
- ;    )
-  ;)
-
 (define (testExpTree lst)
     (cond
         ((= (car lst) 0) lst)
@@ -37,30 +21,19 @@
         (ELSE (testExpTree(list (- (car lst) 1) (cadr lst) (+ 1 (caddr lst)) (cadddr lst))))
      )
 )
-        
-
-
 
 (define (findstats trials)
-    (define x (testExpTree (list trials 0 0 0)))
-    (display x)
+    (define lst (testExpTree (list trials 0 0 0)))
+    (display "Number less than -25: ")
+    (display (cadr lst))
+    (NEWLINE)
+    (display "Number between -25 and 25: ")
+    (display (caddr lst))
+    (NEWLINE)
+    (display "Number greather than 25: ")
+    (display (cadddr lst))
 )
 
+         
 
-;(display "Number less than -25:")
-       ;         (display lessthan)
-        ;        (display "Number between -25 and 25:")
-        ;        (display between)
-         ;       (display "Number greather than 25:")
-         ;       (display greaterthan)
-         
-         
-;(define (testExpTree lst)
-  ;  (cond
-   ;     ((= (car lst) 0) lst)
-    ;    ((< (eval (createTree 3) user-initial-environment) (- 25)) (testExpTree(list (- (car lst) 1) (+ 1 (cadr lst)) (caddr lst) (cdddr lst))))
-     ;   ((> (eval (createTree 3) user-initial-environment) 25) (testExpTree(list (- (car lst) 1) (cadr lst) (caddr lst) (+ 1 (cdddr lst)))))
-    ;    (else (testExpTree((list (- (car lst) 1) (caar list) (+ 1 (caaar lst)) (caaaar lst)))))
-    ; )
-;)
        
