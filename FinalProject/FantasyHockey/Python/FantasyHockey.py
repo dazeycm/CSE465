6cdf987baa69ac65b530c680be0760f9580de4a7
Craig Dazey <dazeycm@miamioh.edu>
2015-05-05 16:59:31 -0400

    Finished fantasy hockey in python

--- FinalProject/FantasyHockey/Python/FantasyHockey.py
+++ FinalProject/FantasyHockey/Python/FantasyHockey.py
@@ -1,1 +1,47 @@
 __author__ = 'Craig'
+
+from tkinter.filedialog import askopenfilename
+
+def createFileOptions():
+    options = {}
+    options['defaultextension'] = '.txt'
+    options['filetypes'] = [('all files', '.*'), ('text files', '.txt')]
+    options['title'] = 'Please open a file with the NHL information you wish to process'
+    return options
+
+def getFileName():
+    options = createFileOptions()
+    name = askopenfilename(**options)
+    return name
+
+'''Returns a string in the form of: NAME scored INT goals!'''
+def getNumGoals(goals, name):
+    if not name in goals:
+        return '{} scored {} goals!'.format(name, 0)
+    else:
+        return '{} scored {} goals!'.format(name, goals[name])
+
+fileName = getFileName()
+file = open(fileName)
+
+goals = {}
+
+for line in file:
+    if "Goal scored" in line:
+        parts = line.split()
+        indexOfName = parts.index('by') + 1
+
+        firstName = parts[indexOfName]
+        lastName = parts[indexOfName + 1].split('(')[0]
+        name = '{} {}'.format(firstName, lastName)
+
+        if not name in goals:
+            goals[name] = 1
+        else:
+            goals[name] += 1
+
+print(getNumGoals(goals, 'Travis Zajac'))
+print(getNumGoals(goals, 'Jaromir Jagr'))
+print(getNumGoals(goals, 'Dmitry Kulikov'))
+print(getNumGoals(goals, 'Dan Ellis'))
+
