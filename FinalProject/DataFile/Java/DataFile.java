5764b759c6e441cafd6148d503e838573d7f01e6
dazeycm <craigdazey@gmail.com>
2015-05-06 21:26:41 -0400

    Added file for java fantasy hockey

--- FinalProject/DataFile/Java/DataFile.java
+++ FinalProject/DataFile/Java/DataFile.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Main {
+public class DataFile {
 	static Student makeStudent(String stats)	{
 		List<String> parts = Arrays.asList(stats.split(" "));
 
