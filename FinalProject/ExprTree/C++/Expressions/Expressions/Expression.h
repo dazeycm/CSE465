8b2aaf1d8e9e1624331103e0166e53a15e493bd4
dazeycm <craigdazey@gmail.com>
2015-05-07 01:49:19 -0400

    Removed old comments

--- FinalProject/ExprTree/C++/Expressions/Expressions/Expression.h
+++ FinalProject/ExprTree/C++/Expressions/Expressions/Expression.h
@@ -19,31 +19,31 @@ struct Node		{
 
 class Expression	{
 public:
-	Expression();										//done
-	Expression(string str);								//done
-	Expression(const Expression &other);				//done
-	Expression &operator = (const Expression &other);	//done
-	~Expression();										//done
-	double evaluate(double x, bool y) const;					//done
+	Expression();										
+	Expression(string str);
+	Expression(const Expression &other);
+	Expression &operator = (const Expression &other);
+	~Expression();
+	double evaluate(double x, bool y) const;
 	static Expression randomExpression(int height);
 	string toString() const;
-	void mutate();										//done
-	int getHeight() const;								//done
+	void mutate();
+	int getHeight() const;
 private:
 	Node *root;
-	Node* parseStatement(string str);					//done
-	int convertToInt(string str) const;					//done and called 0 times in my program :)
-	bool isInteger(const string & s);					//done
-	int findIndexMiddleExp(string str);					//done
-	char getMiddleExp(string str);						//done
-	vector<string> getLeftAndRight(string str);			//done
-	string stripParens(string str);						//done
-	Node* copyHelper(const Node *node);					//done
-	int getHeightFromNode(Node* other) const;			//done
-	double evaluateFromNode(Node* node, double x, bool y) const;//done
-	static Node* addRandomNode(Node* root, int count, int height);//done
-	void changeNode(Node* node);						//done
-	int subTreeNodeCount(Node* node);					//done
-	string getStringSubTree(Node* node, string str) const;//done
-	string nodeToString(Node* node) const;					//done
+	Node* parseStatement(string str);
+	int convertToInt(string str) const;
+	bool isInteger(const string & s);
+	int findIndexMiddleExp(string str);
+	char getMiddleExp(string str);
+	vector<string> getLeftAndRight(string str);
+	string stripParens(string str);
+	Node* copyHelper(const Node *node);
+	int getHeightFromNode(Node* other) const;
+	double evaluateFromNode(Node* node, double x, bool y) const;
+	static Node* addRandomNode(Node* root, int count, int height);
+	void changeNode(Node* node);
+	int subTreeNodeCount(Node* node);
+	string getStringSubTree(Node* node, string str) const;
+	string nodeToString(Node* node) const;
 };
